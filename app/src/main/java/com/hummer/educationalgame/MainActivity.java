df56d5102726db982afbb4dc476bb0bdf165404d
Bleon Uka <k1890506@kcl.ac.uk>
2020-02-07 15:27:23 +0000

    make view xml instead of hard coded

--- app/src/main/java/com/hummer/educationalgame/MainActivity.java
+++ app/src/main/java/com/hummer/educationalgame/MainActivity.java
@@ -4,8 +4,11 @@ import android.app.Activity;
 import android.content.res.AssetManager;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -15,6 +18,30 @@ public class MainActivity extends Activity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(new GamePanel(this));
+        setContentView(R.layout.activity_main);
+        blinkTapToStart();
     }
+    private void blinkTapToStart() {
+        final Handler handler = new Handler();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int timeIntervals = 700;
+                try{Thread.sleep(timeIntervals);}catch (Exception e) {e.printStackTrace();}
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        TextView tapToStart = findViewById(R.id.tap_to_start);
+                        if(tapToStart.getVisibility() == View.VISIBLE){
+                            tapToStart.setVisibility(View.INVISIBLE);
+                        } else {
+                            tapToStart.setVisibility(View.VISIBLE);
+                        }
+                        blinkTapToStart();
+                    }
+                });
+            }
+        }).start();
+    }
+
 }
