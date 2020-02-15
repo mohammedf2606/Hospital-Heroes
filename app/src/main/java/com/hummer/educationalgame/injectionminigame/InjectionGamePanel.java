308d64546cd10943c259d98dcbd12c9d25af655f
FareedFaisal <47261113+FareedFaisal@users.noreply.github.com>
2020-02-15 18:00:28 +0000

    Implement game loop

---
+++ app/src/main/java/com/hummer/educationalgame/injectionminigame/InjectionGamePanel.java
@@ -0,0 +1,74 @@
+package com.hummer.educationalgame.injectionminigame;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class InjectionGamePanel extends SurfaceView implements SurfaceHolder.Callback
+{
+    private InjectionMainThread thread;
+
+    public InjectionGamePanel(Context context)
+    {
+        super(context);
+
+        getHolder().addCallback(this);
+
+        thread = new InjectionMainThread(getHolder(), this);
+
+        setFocusable(true);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height)
+    {
+
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder)
+    {
+        thread = new InjectionMainThread(getHolder(), this);
+
+        thread.setRunning(true);
+        thread.start();
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder)
+    {
+        boolean retry = true;
+        while(true)
+        {
+            try
+            {
+               thread.setRunning(false);
+               thread.join();
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+            retry = false;
+        }
+    }
+
+    //@Override
+    public boolean OnTouchEvent(MotionEvent event)
+    {
+        return super.onTouchEvent(event);
+    }
+
+    public void update()
+    {
+
+    }
+
+    @Override
+    public void draw(Canvas canvas)
+    {
+        super.draw(canvas);
+    }
+}
