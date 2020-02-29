b51b5568ec09e7ce0b0fb4c8045ac1cf9f8de1db
TheMuslimCompSci <45312857+TheMuslimCompSci@users.noreply.github.com>
2020-02-29 18:19:43 +0000

    Add comments

--- app/src/main/java/com/hummer/educationalgame/injectionminigame/InjectionConstants.java
+++ app/src/main/java/com/hummer/educationalgame/injectionminigame/InjectionConstants.java
@@ -2,6 +2,12 @@ package com.hummer.educationalgame.injectionminigame;
 
 import android.content.Context;
 
+/**
+ * InjectionConstant is the class of the game that represents all constants used.
+ *
+ * @author Fareed Faisal
+ * @version 1.0
+ */
 public class InjectionConstants
 {
     public static int SCREEN_WIDTH;
