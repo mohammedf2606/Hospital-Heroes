7d5114596b6fa46c5e5754f437b42d5cced4b428
TheMuslimCompSci <45312857+TheMuslimCompSci@users.noreply.github.com>
2020-02-29 18:19:47 +0000

    Add comments

--- app/src/main/java/com/hummer/educationalgame/injectionminigame/InjectionGameObject.java
+++ app/src/main/java/com/hummer/educationalgame/injectionminigame/InjectionGameObject.java
@@ -2,6 +2,12 @@ package com.hummer.educationalgame.injectionminigame;
 
 import android.graphics.Canvas;
 
+/**
+ * InjectionGameObject is one of 2 main interfaces of the game. 
+ *
+ * @author Fareed Faisal
+ * @version 1.0
+ */
 public interface InjectionGameObject
 {
     public void draw(Canvas canvas);
