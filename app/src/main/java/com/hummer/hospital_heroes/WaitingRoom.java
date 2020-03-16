b1e7a4480741c72a6396fe28bca8a0f7f99952d7
Fahim Mohammed <mohammedf2606@gmail.com>
2020-03-16 13:52:38 +0000

    Add new waiting room assets

--- app/src/main/java/com/hummer/hospital_heroes/WaitingRoom.java
+++ app/src/main/java/com/hummer/hospital_heroes/WaitingRoom.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 
 public class WaitingRoom extends Activity {
 
-    ImageView character_chair, timer;
+    ImageView waiting_room;
     ArrayList<Integer> timers = new ArrayList<>();
     int i = 0;
 
@@ -26,31 +26,36 @@ public class WaitingRoom extends Activity {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.waiting_room);
 
-        timers.add(0, R.drawable.timer_15);
-        timers.add(1, R.drawable.timer_30);
-        timers.add(2, R.drawable.timer_45);
+        timers.add(0, R.drawable.waiting_room_00);
+        timers.add(1, R.drawable.waiting_room_05);
+        timers.add(2, R.drawable.waiting_room_10);
+        timers.add(3, R.drawable.waiting_room_15);
+        timers.add(4, R.drawable.waiting_room_20);
+        timers.add(5, R.drawable.waiting_room_25);
+        timers.add(6, R.drawable.waiting_room_30);
+        timers.add(7, R.drawable.waiting_room_35);
+        timers.add(8, R.drawable.waiting_room_40);
+        timers.add(9, R.drawable.waiting_room_40);
 
-        character_chair = findViewById(R.id.chair_with_character);
-        character_chair.setImageResource(Constants.getPlayerSitting());
-
-        timer = findViewById(R.id.timer);
+        waiting_room = findViewById(R.id.waiting_room);
+        waiting_room.setImageResource(Constants.getPlayerSitting());
 
         final Handler handler = new Handler();
 
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                if(i < 3) {
-                    timer.setImageResource(timers.get(i));
+                if(i < 9) {
+                    waiting_room.setImageResource(timers.get(i));
                     i++;
-                    handler.postDelayed(this, 2000);
+                    handler.postDelayed(this, 1000);
                 }
                 else {
                     Intent injection_minigame = new Intent(WaitingRoom.this, InjectionMainMenu.class);
                     startActivity(injection_minigame);
                 }
             }
-        }, 2000);
+        }, 1000);
     }
 
 
