e3d8d9d038979e37e76258ac3bca74f524e5ae48
TheMuslimCompSci <fareed.faisal@kcl.ac.uk>
2020-04-10 11:03:44 +0100

    add injection game tests

---
+++ app/src/androidTest/java/com/hummer/hospital_heroes/food_minigame/MainMenuForFoodTest.java
@@ -0,0 +1,71 @@
+package com.hummer.hospital_heroes.food_minigame;
+
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.view.View;
+
+import com.hummer.hospital_heroes.R;
+import com.hummer.hospital_heroes.injection_minigame.InjectionMainActivity;
+import com.hummer.hospital_heroes.injection_minigame.InjectionMainMenu;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.rule.ActivityTestRule;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+import static org.junit.Assert.*;
+
+public class MainMenuForFoodTest {
+    @Rule
+    public ActivityTestRule<MainMenuForFood> mActivityTestRule = new ActivityTestRule<>(MainMenuForFood.class);
+    private MainMenuForFood mActivity = null;
+    Instrumentation.ActivityMonitor monitor = getInstrumentation().addMonitor(MainGameActivity.class.getName(), null, false);
+
+    @Before
+    public void setUp() throws Exception
+    {
+        mActivity = mActivityTestRule.getActivity();
+    }
+
+    @Test
+    public void onCreate()
+    {
+        View view = mActivity.findViewById(R.id.playButton);
+        assertNotNull(view);
+    }
+
+    @Test
+    public void nextScene()
+    {
+        assertNotNull(mActivity.findViewById(R.id.playButton));
+        onView(withId(R.id.playButton)).perform(click());
+        Activity nextScene = getInstrumentation().waitForMonitorWithTimeout(monitor, 5000);
+        assertNotNull(nextScene);
+        nextScene.finish();
+    }
+
+    @Test
+    public void launch()
+    {
+        Intents.init();
+        Espresso.onView(withId(R.id.playButton)).perform(click());
+        intended(hasComponent(MainGameActivity.class.getName()));
+        Intents.release();
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+        mActivity = null;
+    }
+}
